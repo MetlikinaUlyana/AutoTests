ad2a1a1909f7bb56f6995f4c3a05983b47d811be
Ulyana Metlikina <maily17@yandex.ru>
2021-04-13 12:50:44 +0500

    My first autotests

---
+++ ParrotNameTests/ParrotNameTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ParrotNameTests
+{
+    public class HomeworkTests
+    {
+        public ChromeDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+
+        private const string email = "test@mail.ru";
+        private const string siteUrl = "https://qa-course.kontur.host/selenium-practice";
+        private readonly By boyRadioButtonLocator = By.Id("boy");
+        private readonly By girlRadioButtonLocator = By.Id("girl");
+        private readonly By emailInputLocator = By.Name("email");
+        private readonly By buttonLocator = By.Id("sendMe");
+        private readonly By emailResultLocator = By.ClassName("your-email");
+        private readonly By resultTextLocator = By.ClassName("result-text");
+        private readonly By errorMessageLocator = By.ClassName("form-error");
+
+        [Test]
+        public void ComputerSite_CheckGirl_FillFormWithEmail_Success()
+        {
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(girlRadioButtonLocator).Click();
+            driver.FindElement(emailInputLocator).SendKeys(email);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Хорошо, мы пришлём имя для вашей девочки на e-mail:",
+                driver.FindElement(resultTextLocator).Text, "Список имен сформирован для девочки");
+            Assert.AreEqual(email, driver.FindElement(emailResultLocator).Text, "Создали заявку на список имен для девочки на указанный e-mail");
+        }
+
+        [Test]
+        public void ComputerSite_CheckBoy_FillFormWithEmail_Success()
+        {
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(boyRadioButtonLocator).Click();
+            driver.FindElement(emailInputLocator).SendKeys(email);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Хорошо, мы пришлём имя для вашего мальчика на e-mail:",
+                driver.FindElement(resultTextLocator).Text, "Список имен сформирован для мальчика");
+            Assert.AreEqual(email, driver.FindElement(emailResultLocator).Text, "Создали заявку на список имен для мальчика на указанный e-mail");
+        }
+
+        [Test]
+        public void ComputerSite_ChangeGender_EmailIsNotCleared()
+        {
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(boyRadioButtonLocator).Click();
+            driver.FindElement(emailInputLocator).SendKeys(email);
+            driver.FindElement(girlRadioButtonLocator).Click();
+            
+            Assert.AreEqual(email, driver.FindElement(emailInputLocator).GetAttribute("value"), "После смены пола животного поле e-mail не очистилось");
+        }
+
+        [Test]
+        public void ComputerSite_LeaveEmailFormEmpty_EmptyInputErrorMessage()
+        {
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(emailInputLocator).SendKeys(string.Empty);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Введите email", driver.FindElement(errorMessageLocator).Text, "Уведомление о незаполненном поле e-mail");
+        }
+        
+        [Test]
+        public void ComputerSite_TooLongEmailLength_EmailLengthErrorMessage()
+        {
+            var name = RandomGenerate(313);
+            var email = $"{name}@mail.ru";
+
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(emailInputLocator).SendKeys(email);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Некорректный email", driver.FindElement(errorMessageLocator).Text, "Уведомление о некорректном e-mail");
+        }
+
+        [Test]
+        [TestCase("@mail.ru")]
+        [TestCase("zlatmail.ru")]
+        [TestCase("zlat@mail.")]
+        [TestCase("zlat@.ru")]
+        public void ComputerSite_FillWithInvalidEmail_IncorrectEmailErrorMessage(string email)
+        {
+            driver.Navigate().GoToUrl(siteUrl);
+
+            driver.FindElement(emailInputLocator).SendKeys(email);
+            driver.FindElement(buttonLocator).Click();
+
+            Assert.AreEqual("Некорректный email", driver.FindElement(errorMessageLocator).Text, "Уведомление о некорректном e-mail");
+        }
+
+        private static string RandomGenerate(int length)
+        {
+            var stringBuilder = new StringBuilder();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrsqtuvwxyz";
+            var random = new Random();
+            for (var i = 0; i < length; i++)
+                stringBuilder.Append(chars[random.Next(chars.Length)]);
+
+            return stringBuilder.ToString();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
+
+
